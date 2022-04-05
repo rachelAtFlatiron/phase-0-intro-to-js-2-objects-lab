dc42ceae59d8e7c722da2f77b9957f08b5e38328
Rachel Katz <rachel.katz@flatironschool.com>
2022-04-05 14:13:12 -0400

    commit tree 3528b48cfd3f128095505bd1ea57877325f47bba

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Write your solution in this file!
+
+let employee = {
+    name: "first last",
+    streetAddress: 'place on place st'
+} 
+
+let updateEmployeeWithKeyAndValue = (employee, key, value) => {
+    let newEmployee = {...employee}
+    newEmployee[key] = value;
+    return newEmployee;
+}
+
+let destructivelyUpdateEmployeeWithKeyAndValue = (employee, key, value) => {
+    employee[key] = value;
+    return employee;
+}
+
+let deleteFromEmployeeByKey = (employee, key) => {
+    let newEmployee = {...employee};
+    delete newEmployee[key] 
+    return newEmployee;   
+}
+
+let destructivelyDeleteFromEmployeeByKey = (employee, key) => {
+    delete employee[key];
+    return employee;
+}
